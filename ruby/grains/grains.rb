2464cf1e2568459d29e672b898795988f237a418
Thomas F <taforres@asu.edu>
2018-05-28 13:29:03 -0700

    Add feedback to grains problem

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,8 +1,11 @@
-class Grains 
+class Grains
+  BOARD_SIZE = 64
   def self.square(n)
-    raise ArgumentError if n < 1 || n > 64
+    raise ArgumentError if n < 1 || n > BOARD_SIZE
     square_helper(2, n - 1)
   end
+
+  private_class_method
 
   def self.square_helper(x, n)
     return 1 if n.zero?
